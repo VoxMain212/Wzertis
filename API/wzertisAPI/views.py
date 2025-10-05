889c28b98649ffcf0224d8c111761ae2c3f45dd8
VoxMain212 <777ferb@gmail.com>
2025-10-06 01:14:40 +0500

    Cool

--- API/wzertisAPI/views.py
+++ API/wzertisAPI/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from .models import AuthorInfo, ArtGroup, Artwork, Appeal
 from .serializers import AuthorInfoSerializer, ArtGroupSerializer, ArtworkSerializer, AppealSerializer
+
+
+def index(request):
+    return render(request, 'index.html')
 
 @api_view(['GET'])
 def author_info_api(request):
